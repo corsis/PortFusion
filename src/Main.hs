af4b2c9bcd8e3630a7f8f7051c1fff937a4149cb
Cetin Sert <cetin.sert@gmail.com>
2012-08-15 01:42:04 +0200

    using ($!) instead of ($) everywhere

--- src/Main.hs
+++ src/Main.hs
@@ -47,7 +47,7 @@ import GHC.Conc (numCapabilities)
 type Seconds = Int
 secs     :: Int -> Seconds;                  secs         = (* 1000000)
 wait     :: Seconds -> IO ();                wait         = threadDelay . secs
-schedule :: Seconds -> IO () -> IO ThreadId; schedule s a = forkIO $ wait s >> a
+schedule :: Seconds -> IO () -> IO ThreadId; schedule s a = forkIO $! wait s >> a
 
 {-# INLINE (<>)  #-}; (<>) :: ByteString -> ByteString -> ByteString; (<>)   = B.append
 {-# INLINE (//)  #-}; (//) :: a -> (a -> b) -> b;                     x // f = f x
@@ -56,7 +56,7 @@ schedule :: Seconds -> IO () -> IO ThreadId; schedule s a = forkIO $ wait s >> a
 a =>> f = do r <- a; _ <- f r; return r
 
 type ErrorIO = IO
-att    :: IO a  -> IO (Maybe a);       att    a = tryWith (const $ return Nothing) (Just <$> a)
+att    :: IO a  -> IO (Maybe a);       att    a = tryWith (const $! return Nothing) (Just <$> a)
 tryRun :: IO () -> IO ();              tryRun a = tryWith (\x -> do print x; wait 2) a
 (???)  :: ErrorIO a -> [IO a] -> IO a; e ??? as = foldr (?>) e as
   where x ?> y = x `X.catch` (\(_ :: X.SomeException) -> y)
@@ -64,7 +64,7 @@ tryRun :: IO () -> IO ();              tryRun a = tryWith (\x -> do print x; wai
 newtype LiteralString = LS ByteString
 instance IsString LiteralString where fromString  = LS . B.pack
 instance Show     LiteralString where show (LS x) = B.unpack x
-instance Read     LiteralString where readsPrec p s = map (\(s,r) -> (LS s,r)) $ readsPrec p s
+instance Read     LiteralString where readsPrec p s = map (\(s,r) -> (LS s,r)) $! readsPrec p s
 
 -----------------------------------------------------------------------------------------------PEERS
 
@@ -73,13 +73,13 @@ data FusionLink = FusionLink (Maybe SockAddr) (Maybe Port    ) (Maybe SockAddr) 
 data ProtocolException = Loss PeerLink | Silence [SockAddr]           deriving (Typeable,Show)
 instance X.Exception ProtocolException where
 
-(<:) :: Show a => Socket -> a -> IO (); s <: a = s `sendAll` ((B.pack . show $ a) <> "\r\n")
+(<:) :: Show a => Socket -> a -> IO (); s <: a = s `sendAll` ((B.pack . show $! a) <> "\r\n")
 
 (<@>)   :: Socket ->           IO PeerLink
-(<@>)   s = PeerLink <$> (att $ getSocketName s) <*> (att $ getPeerName s)
+(<@>)   s = PeerLink <$> (att $! getSocketName s)<*>(att $! getPeerName s)
 
 (@>-<@) :: Socket -> Socket -> IO FusionLink
-a @>-<@ b = FusionLink <$> (att $ getPeerName a) <*> (att $ socketPort b) <*> (att $ getPeerName b)
+a @>-<@ b = FusionLink <$> (att $! getPeerName a)<*>(att $! socketPort  b)<*>(att $! getPeerName b)
 
 (@<) :: AddrPort -> IO Socket
 (@<) ap = do
@@ -97,7 +97,7 @@ h .@. p = getAddrInfo hint host port >>= \as -> e as ??? map c as
   where hint = Just $! defaultHints { addrSocketType = Stream }
         host = Just $! B.unpack h
         port = Just $! show     p
-        e as = X.throwIO . Silence $ map addrAddress as
+        e as = X.throwIO . Silence $! map addrAddress as
         c a  = do s <-      socket (addrFamily  a) Stream 0x6 =>> configure
                   r <- s `connect`  addrAddress a // timeout (secs 3)
                   case r of
@@ -106,7 +106,7 @@ h .@. p = getAddrInfo hint host port >>= \as -> e as ??? map c as
 
 configure :: Socket -> IO ()
 configure s = m RecvBuffer c >> m SendBuffer c >> setSocketOption s KeepAlive 1
-   where m o u = do v <- getSocketOption s o; when (v < u) $ setSocketOption s o u
+   where m o u = do v <- getSocketOption s o; when (v < u) $! setSocketOption s o u
          c     = fromIntegral chunk
 
 chunk :: ChunkSize
@@ -123,9 +123,9 @@ data Peer = Peer !Socket !Handle
 class    Disposable a       where (✖) :: a -> IO ()
 instance Disposable Socket  where
   (✖) s = do
-    try_ $ print . (Close :.:) =<< (s <@>)
-    try_ $ shutdown s ShutdownBoth
-    try_ $ sClose   s
+    try_ $! print . (Close :.:) =<< (s <@>)
+    try_ $! shutdown s ShutdownBoth
+    try_ $! sClose   s
 instance Disposable Peer          where (✖) (Peer s h) = do (s ✖); (h ✖)
 instance Disposable Handle        where (✖) = try_ . hClose
 instance Disposable (Ptr       a) where (✖) = free
@@ -136,7 +136,7 @@ instance Disposable (StablePtr a) where (✖) = freeStablePtr
 type Host = ByteString
 type Port = PortNumber
 
-instance Read Port where readsPrec p s = map (\(i,r) -> (fromInteger i,r)) $ readsPrec p s
+instance Read Port where readsPrec p s = map (\(i,r) -> (fromInteger i,r)) $! readsPrec p s
 
 data AddrPort = !Host :@: !Port
 instance Show AddrPort where
@@ -144,16 +144,16 @@ instance Show AddrPort where
     where f  a = if B.elem ':' a then "["++show (LS a)++"]" else show (LS a)
 instance Read AddrPort where
   readsPrec p s =
-    case reverse $ elemIndices ':' s of { [] -> all s; (0:_) -> all $ drop 1 s; (i:_) -> one i s }
-    where all   s =  readsPrec p s >>= \(p, s') -> return $ ("" :@: p, s')
-          one i s =  do
+    case reverse $! elemIndices ':' s of { [] -> all s; (0:_) -> all $! drop 1 s; (i:_) -> one i s }
+    where all   s = readsPrec p s >>= \(p, s') -> return $! ("" :@: p, s')
+          one i s = do
             let (x,y) = splitAt i s // \(a,b) -> (dropWhile isSpace a, b)
             (a,_) <- readsPrec p $! "\"" ++ filter (\c -> c /= '[' && ']' /= c) x ++ "\""
             (p,r) <- readsPrec p $! tail y
-            return $ (a :@: p, r)
+            return $! (a :@: p, r)
 
 faf :: Family -> LiteralString
-faf x  = LS $! case x of { AF_INET6 -> sf; AF_UNSPEC -> sf; AF_INET -> "IPv4"; _-> B.pack $ show x }
+faf x = LS $! case x of { AF_INET6 -> sf; AF_UNSPEC -> sf; AF_INET -> "IPv4"; _-> B.pack $! show x }
   where sf = "IPv6(+4?)"
 
 (?:) :: AddrPort -> IO (Family, SockAddr)
@@ -193,23 +193,23 @@ copyright = "(c) 2012 Cetin Sert. All rights reserved."
 build     = __OS__ <> " - " <> __ARCH__ <>  " [" <> __TIMESTAMP__ <> "]"
 
 main :: IO ()
-main = withSocketsDo $ tryWith (const . print $ LS "INVALID SYNTAX") $ do
+main = withSocketsDo $! tryWith (const . print $! LS "INVALID SYNTAX") $! do
   mapM_ B.putStrLn [ "\n", name, copyright, "", build, "\n" ]
   tasks <- parse <$> getArgs
   when   (null tasks) $! mapM_ B.putStrLn [ "  Documentation: http://fusion.corsis.eu", "",""]
   unless (null tasks) $! do
-    when zeroCopy              $ print (LS "zeroCopy"       , zeroCopy       )
-    when (numCapabilities > 1) $ print (LS "numCapabilities", numCapabilities)
+    when zeroCopy              $! print (LS "zeroCopy"       , zeroCopy       )
+    when (numCapabilities > 1) $! print (LS "numCapabilities", numCapabilities)
     mapM_ (forkIO . run) tasks
     void Prelude.getChar
 
 
 parse :: [String] -> [Task]
-parse [         "]", ap, "["         ] = [(:><:) $ read ap                                         ]
+parse [         "]", ap, "["         ] = [(:><:) $! read ap                                        ]
 parse [ lp, lh, "-", fp, fh, "[", ap ] = [(read lp, B.pack lh) :-<: ((read fp, B.pack fh),read ap) ]
 parse [ ap, "]", fh, fp, "-", rh, rp ] = [read ap :>-: ((B.pack fh, read fp), (B.pack rh, read rp))]
 parse [ ap, "]",         "-", rh, rp ] = [read ap :>=:                        (B.pack rh, read rp) ]
-parse m = concatMap parse $ map (map B.unpack . filter (not . B.null) . B.split ' ' . B.pack) m
+parse m = concatMap parse $! map (map B.unpack . filter (not . B.null) . B.split ' ' . B.pack) m
 
 ----------------------------------------------------------------------------------------------VECTOR
 
@@ -258,15 +258,15 @@ initPortVectors = modifyMVar_ portVectorsInitialized $! \initialized ->
 (|<>|) :: (MVar ThreadId -> IO ()) -> (MVar ThreadId -> IO ()) -> IO ()
 a |<>| b = do
   ma <- newEmptyMVar ; mb <- newEmptyMVar
-  ta <- forkIO $ a mb; tb <- forkIO $ b ma
+  ta <- forkIO $! a mb; tb <- forkIO $! b ma
   putMVar       ma ta; putMVar       mb tb
 
 (-✖-) :: Peer -> AddrPort -> MVar ThreadId -> IO ()
 (o@(Peer s _) -✖- rp) t = do
   l <- (s <@>)
   let n x = do (o ✖); (rp -✖); takeMVar t >>= (`throwTo` x)
-  let f x = do maybe (n x) (const $ return ()) $ (X.fromException x :: Maybe X.AsyncException)
-  tryWith f $ do recv s 0; f . X.toException $ Loss l
+  let f x = do maybe (n x) (const $! return ()) $! (X.fromException x :: Maybe X.AsyncException)
+  tryWith f $! do recv s 0; f . X.toException $! Loss l
 
 -----------------------------------------------------------------------------------------------TASKS
 
@@ -275,18 +275,18 @@ run ((:><:) fp) = do
 
   f <- (fp @<)
 
-  forever $ void . forkIO . serve =<< (f !<@)
+  forever $! void . forkIO . serve =<< (f !<@)
 
    where
 
     serve :: Peer -> IO ()
     serve o@(Peer s h) = do
-      tryWith (const (o ✖)) $ do                         -- any exception disposes o
+      tryWith (const (o ✖)) $! do                        -- any exception disposes o
         q <- read . B.unpack <$> B.hGetLine h
         print . (:.:) (Receive q) =<< (s <@>)
         case q of
-          (:-<-:)    rp -> o -<-      rp
-          (:->-:) rh rp -> o ->- rh $ rp
+          (:-<-:)    rp -> o -<-       rp
+          (:->-:) rh rp -> o ->- rh $! rp
           (:?)          -> s <: LS build |> (o ✖)
           Run task      -> run task      |> (o ✖)
 
@@ -299,12 +299,12 @@ run ((:><:) fp) = do
         tryWith (const f) $! do
           c <- (r !<@); f
           l `sendAll` "+"
-          o >-< c $ (rp -✖)
+          o >-< c $! (rp -✖)
 
     (->-) :: Peer -> Host -> Port -> IO ()
     (o ->- rh) rp = do
       e <- rh ! rp
-      o >-< e $ return ()                                -- any exception disposes o ^
+      o >-< e $! return ()                               -- any exception disposes o ^
 
 
 --- :: Task -> IO () - distributed reverse
@@ -317,10 +317,10 @@ run ((lp,lh) :-<: ((fp,fh),rp)) = do
     s <: m
     _ <- s `recv` 1
 
-    void . forkIO $ do
+    void . forkIO $! do
 
       e <- lh ! lp `X.onException` (f ✖)
-      f >-< e $ return ()
+      f >-< e $! return ()
 
 
 --- :: Task -> IO () - distributed forward
@@ -328,17 +328,17 @@ run (lp :>-: ((fh,fp),(rh,rp))) = do
 
   l <- (lp @<)
 
-  forever . tryRun $ do
+  forever . tryRun $! do
 
     c <- (l !<@)
 
-    void . forkIO . tryWith (const (c ✖)) $ do
+    void . forkIO . tryWith (const (c ✖)) $! do
 
       f@(Peer s _) <- fh ! fp
       let m = (:->-:) rh rp
       print . (:.:) (Send m) =<< (s <@>)
       s <: m
-      f >-< c $ return ()
+      f >-< c $! return ()
 
 
 --- :: Task -> IO () - direct forward
@@ -346,22 +346,22 @@ run (lp :>=: (rh, rp)) = do
 
   l <- (lp @<)
 
-  forever . tryRun $ (l !<@) `X.bracketOnError` (✖) $ \c -> do
+  forever . tryRun $! (l !<@) `X.bracketOnError` (✖) $! \c -> do
 
     r <- rh ! rp
-    r >-< c $ return ()
+    r >-< c $! return ()
 
 ----------------------------------------------------------------------------------------------SPLICE
 
 (>-<) :: Peer -> Peer -> ErrorIO () -> IO ()
 (a@(Peer as _) >-< b@(Peer bs _)) h = do
   !t <- as @>-<@ bs
-  print $ Establish ::: t
+  print $! Establish ::: t
   !m <- newMVar True
-  let p = print $ Terminate ::: t
-  let j = modifyMVar_ m $ \v -> do when v (do p; (a ✖); (b ✖); h); return False
-  a >- b $ j; b >- a $ j
+  let p = print $! Terminate ::: t
+  let j = modifyMVar_ m $! \v -> do when v (do p; (a ✖); (b ✖); h); return False
+  a >- b $! j; b >- a $! j
 
 (>-) :: Peer -> Peer -> ErrorIO () -> IO ()
 (Peer as ah >- Peer bs bh) j =
-  void . forkIO . tryWith (const j) $ splice chunk (as, Just ah) (bs, Just bh)
+  void . forkIO . tryWith (const j) $! splice chunk (as, Just ah) (bs, Just bh)
