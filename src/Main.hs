5337fdf488da66c1be2cd997e3da83cf6e30febb
Cetin Sert <cetin.sert@gmail.com>
2012-08-16 00:56:44 +0200

    ...

--- src/Main.hs
+++ src/Main.hs
@@ -188,7 +188,7 @@ data Request = (:-<-:)    AddrPort
 ------------------------------------------------------------------------------------------------MAIN
 
 name, copyright, build :: ByteString
-name      = "CORSIS PortFusion    ( ]-[ayabusa 1.3.0 )"
+name      = "CORSIS PortFusion    ( ]-[ayabusa 1.2.1 )"
 copyright = "(c) 2012 Cetin Sert. All rights reserved."
 build     = __OS__ <> " - " <> __ARCH__ <>  " [" <> __TIMESTAMP__ <> "]"
 
